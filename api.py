110a6ae8ca010fa0a371ba4a6ed228724c5033dd
Cristian Lopez <clopezm20@correo.uss.cl>
2026-05-26 22:52:35 -0400

    Add files via upload

--- api.py
+++ api.py
@@ -2,7 +2,9 @@ import requests
 
 URL = "https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php"
 
+HEADERS = {"User-Agent": "Mozilla/5.0"}
+
 def obtener_datos():
-    r = requests.get(URL)
+    r = requests.get(URL, headers=HEADERS)
     r.raise_for_status()
     return r.json()
